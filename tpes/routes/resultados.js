72a191fb571772215e8ce9d522aa5530899d1853
Bernardo Roberto <98844794+bernardoras@users.noreply.github.com>
2023-10-16 03:02:28 -0300

    Update resultados.js
    Assim como criar_eleicao.html e eleicao.html, resultados.html é referenciado em perfil.html.

--- tpes/routes/resultados.js
+++ tpes/routes/resultados.js
@@ -3,19 +3,6 @@ const path = require('path');
 const router = express.Router();
 const pool = require("./../dbConnection");
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-
-  if (req.headers.token == 'we221e32342342342342342weaeaEWWAWE@4123') {
-
-    res.sendFile(path.join(__dirname, '../public', 'resultados.html'));
-
-  } else {
-
-    return res.send("please log in");
-  }
-});
-
 router.get('/perfilData', function(req, res, next) {
 
     if (req.headers.token == 'we221e32342342342342342weaeaEWWAWE@4123') {
